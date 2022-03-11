f3552a59d78683b709557ad063797e7c437f4340
AntsoTech <95869782+AntsoTech@users.noreply.github.com>
2022-03-11 12:18:14 +0100

    Add comments to index.js

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
+//Ask for the player name and the number
 const playerName = prompt('Quel est ton nom ?');
 const random = parseInt(Math.floor(Math.random()*100+1));
 let playerNumber = parseInt(prompt('Quel est ton nombre ?'));
+
+//Add a loop to make sure the number is beetween 0 and 100
 while (playerNumber > 100 || playerNumber < 0) {
     playerNumber = parseInt(prompt('Quel est ton nombre ? On a dit entre 0 et 100 ! BOULET !'))
 }
 
-console.log(playerName);
-console.log(playerNumber);
-
+//Add a loop and condition to indicate to the player if it's less or more
 while (playerNumber !== random) {
     if (random < playerNumber) {
-        console.log(`it's less`);
         playerNumber = parseInt(prompt(`it's less, Quel est ton nombre ?`))
     }
     if (random > playerNumber) {
-        console.log(`it's more`);
         playerNumber = parseInt(prompt(`it's more, Quel est ton nombre ?`))
     }
 }
 
-alert('Vous avez trouvé :' + random);
+//Add a winning message
+alert('Vous avez trouvé : ${random}');
